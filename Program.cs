4b78855b8169649510290a3626621740adb32dcf
jules <jules.white1@protonmail.com>
2025-07-15 15:02:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var file = File.ReadAllText(args[0]);
+
+var dict = new Dictionary<char, int>();
+
+var stripped = new string(file.Where(x => !char.IsAsciiLetter(x)).ToArray());
+
+foreach(var c in stripped) {
+    if(dict.ContainsKey(c)) {
+        dict[c]++;
+        continue;
+    }
+    dict.Add(c, 0);
+}
+
+var ordered = dict.ToList().OrderBy(x => x.Value).ToList();
+
+foreach(var entry in ordered) {
+    Console.WriteLine($"'{entry.Key}', {entry.Value}");
+}
